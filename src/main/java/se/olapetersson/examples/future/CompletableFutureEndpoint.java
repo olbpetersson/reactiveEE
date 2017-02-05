03458c0cadc97dda2ef76f2a206a3421d22603c8
Ola Petersson <olbpetersson@gmail.com>
2017-02-05 20:33:23 +0100

    removed the mes

--- src/main/java/se/olapetersson/examples/future/CompletableFutureEndpoint.java
+++ src/main/java/se/olapetersson/examples/future/CompletableFutureEndpoint.java
@@ -2,29 +2,22 @@ package se.olapetersson.examples.future;
 
 import javax.annotation.Resource;
 import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import java.util.logging.Logger;
 
-/**
- * Created by ola on 2016-01-28.
- */
 @Path("future")
 public class CompletableFutureEndpoint {
-    Logger logger = Logger.getLogger(CompletableFutureEndpoint.class.getName());
 
     @GET
     @Path("/sync")
-    public String syncGet() throws InterruptedException {
+    public String syncGet() {
         String foo = getFoo();
         return foo;
     }
-
-    @Resource
-    private ManagedExecutorService executorService;
-
 
     @GET
     @Path("/async")
@@ -59,19 +52,18 @@ public class CompletableFutureEndpoint {
         return "I'm Foo";
     }
 
-    private String getTweetFromBar() {
-        /**
-         * @author The Summer Intern
-         *
-         * @not Ola
-         */
-        sleepThread(500);
-        return "I'm bar";
-    }
 
-    private String combineStrings(String tweetFromFoo, String tweetFromBar) {
-        return tweetFromFoo + " and " +tweetFromBar;
-    }
+
+
+
+
+
+
+
+
+
+
+
 
     private void sleepThread(long ms){
         try {
